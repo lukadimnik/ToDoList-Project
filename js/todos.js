7d4fd93da4cbaa0ea56c7e22bed86f3e46500763
Luka Dimnik <luka.dimnik88@gmail.com>
2019-06-28 15:16:04 +0300

    update

--- js/todos.js
+++ js/todos.js
@@ -1,1 +1,29 @@
-alert("cood");
+//check off specified todos by click
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("ul").on("click", "span", function(event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append(
+      "<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>"
+    );
+  }
+});
+
+$(".fa-trash-alt").click(function() {
+  $("input[type='text']").fadeToggle();
+});
